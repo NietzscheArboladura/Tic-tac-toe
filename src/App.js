d42cf3f52fff5df04464d465cdaa083ae8c8dac6
Nietzsche Arboladura <93897510+NietzscheArboladura@users.noreply.github.com>
2022-12-01 21:15:45 +0800

    wew

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { CDBFooter, CDBBtn, CDBIcon, CDBBox } from "cdbreact";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+//import NavDropdown from "react-bootstrap/NavDropdown";
 
 function App() {
   return (
